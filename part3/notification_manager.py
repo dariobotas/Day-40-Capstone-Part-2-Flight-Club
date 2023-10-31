bd401256dc3cb6e0befcb74b381fcd39af636128
dariobotas <dario.botas@gmail.com>
2023-10-31 13:05:54 +0000

    lesson step5

--- part3/notification_manager.py
+++ part3/notification_manager.py
@@ -1,4 +1,5 @@
 import os
+import smtplib as mail
 
 from twilio.rest import Client
 
@@ -6,7 +7,8 @@ TWILIO_SID = os.environ["TWILIO_ACCOUNT_SID"]
 TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
 TWILIO_VIRTUAL_NUMBER = '+14143166728'
 TWILIO_VERIFIED_NUMBER = '+351912680064'
-
+EMAIL_FROM = os.environ["MY_EMAIL"]
+PASSWORD = os.environ["MY_PASSWORD"]
 
 class NotificationManager:
 
@@ -21,3 +23,12 @@ class NotificationManager:
         )
         # Prints if successfully sent.
         print(message.sid)
+    
+    def send_emails(self, email, message):
+      connection = mail.SMTP('smtp.gmail.com', 587)
+      connection.starttls()
+      connection.login(EMAIL_FROM, PASSWORD)
+      connection.sendmail(from_addr=EMAIL_FROM,
+                         to_addrs=email,
+                         msg=f"Subject: Notification from Part 3 \n\n{message}")
+      connection.quit()
